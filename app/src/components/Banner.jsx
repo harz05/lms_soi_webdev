b47b174e4d745cbbe46700ddb7038152f067c3b4
Harsh Chauhan <73985490+harz05@users.noreply.github.com>
2024-07-19 12:22:15 +0530

    Update Banner.jsx

--- app/src/components/Banner.jsx
+++ app/src/components/Banner.jsx
@@ -7,10 +7,10 @@ const Banner = () => {
       <div className='flex flex-col md:flex-row justify-between items-center gap-12 py-40 w-full'>
         <div className='md:w-1/2 space-y-8'>
           <h2 className='text-5xl font-bold leading-snug text-black'>
-            Welcome to <span className='text-blue-700'>IIT Dholakpur LMS</span>
+            Welcome to <span className='text-blue-700'>IIT Dharwad LMS</span>
           </h2>
           <p className="md:w-full font-semibold">
-            The Indian Institute of Technology Dholakpur (IIT Dholakpur) is a premier institution known for its excellence in education, research, and innovation. Our Library Management System (LMS) is designed to provide seamless access to our extensive collection of books, journals, and digital resources, supporting the academic and research needs of our students and faculty.
+            The Indian Institute of Technology Dharwad (IIT Dharwad) is a premier institution known for its excellence in education, research, and innovation. Our Library Management System (LMS) is designed to provide seamless access to our extensive collection of books, journals, and digital resources, supporting the academic and research needs of our students and faculty.
           </p>
         </div>
         <div className='md:w-1/2'>
